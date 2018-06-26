13075b67b3708d3faaa0e76455ff2cd73d8291f0
yanyzy <1181862765@qq.com>
2018-06-26 23:42:11 +0800

    开始了第六章

--- src/unit5/ReflectivePractice/Test.java
+++ src/unit5/ReflectivePractice/Test.java
@@ -1,15 +1,31 @@
 package unit5.ReflectivePractice;
 
 
+import unit5.ExtendsPractice.Employee;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Date;
 
 public class Test {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
 
+        testInvoke();
     }
 
 
+    /**
+     * Method.invoke可以调用某个类的该方法
+     * 当方法为静态方法，第一个参数为null
+     */
+    private static void testInvoke() throws NoSuchMethodException, IllegalAccessException, InstantiationException, InvocationTargetException {
+        Method setAge = Employee.class.getMethod("setAge",int.class);
+        Method getAge = Employee.class.getDeclaredMethod("getAge");
+        Employee employee = Employee.class.newInstance();
+        setAge.invoke(employee,10);
+        System.out.println("age= "+getAge.invoke(employee));
+    }
 
     /**
      * 使用newInstance创建类实例
